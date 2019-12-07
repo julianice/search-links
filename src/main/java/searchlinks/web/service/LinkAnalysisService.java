0c1ccff15b1755d8f12ed5c84fa0aa0b168d4b18
Julia Shipitsyna <ju.shipitsyna@gmail.com>
2019-12-07 17:35:25 +0300

    first version with future

--- src/main/java/searchlinks/web/service/LinkAnalysisService.java
+++ src/main/java/searchlinks/web/service/LinkAnalysisService.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import searchlinks.entities.Link;
 import searchlinks.entities.Page;
@@ -12,9 +14,13 @@ import searchlinks.entities.Site;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 @Service
 public class LinkAnalysisService {
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    Logger logger = LoggerFactory.getLogger(LinkAnalysisService.class);
+
 
     public List<Page> getPages(Site site) {
         List<Page> pages = new ArrayList<>();
@@ -24,19 +30,29 @@ public class LinkAnalysisService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Elements elements = doc.select("loc");
-        for (int i = 0; i < 10; i++) {
-            Page page = new Page(site, elements.get(i).text());
-            pages.add(page);
+
+        List<Future<Page>> futures = new ArrayList<>();
+        for (Element element : doc.select("loc")) {
+            Future<Page> futurePage = executorService.submit(() -> {
+                Page page = new Page(site, element.text());
+                return page;
+            });
+            futures.add(futurePage);
         }
-        //пока ограничила 10 страницами
-//        for (Element e : doc.select("loc")) {
-//            pages.add(new Page(this, e.text()));
-//        }
+
+        for (Future<Page> futurePage : futures) {
+            if (futurePage.isDone()) {
+                try {
+                    pages.add(futurePage.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.error("Getting page was interrupted");
+                }
+            }
+        }
         return pages;
     }
 
-    public List<Link> getLinks(Site site, Page page) {
+    public List<Link> getLinks(Site site, Page page) throws ExecutionException, InterruptedException {
         List<Link> links = new ArrayList<>();
         Document doc = null;
         try {
@@ -44,9 +60,21 @@ public class LinkAnalysisService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        List<Future<Link>> futures = new ArrayList<>();
+
         for (Element e : doc.select("a[href]")) {
             if (e.attr("href").contains("http")) {
-                links.add(new Link(site, page, e.attr("href")));
+                Future<Link> future = executorService.submit(() -> {
+                    Link link = new Link(site, page, e.attr("href"));
+                    return link;
+                });
+                futures.add(future);
+            }
+        }
+
+        for (Future<Link> linkFuture : futures) {
+            if (linkFuture.isDone()) {
+                links.add(linkFuture.get());
             }
         }
         return links;
